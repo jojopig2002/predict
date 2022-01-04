7a0c8f50741dfcc5466bc74518494ae9047606cc
jojopig2002 <jojopig2002@hotmail.com>
2022-01-04 23:28:01 +0800

    tech: predict

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import datetime
-import math
 
-import matplotlib.pyplot as plt
 import pandas as pd
 import pymysql as pymysql
 from sklearn.linear_model import LinearRegression
@@ -20,57 +18,81 @@ def run():
     conn = pymysql.connect(host=host, user=user, password=password, db=dbname)
     origDf = pd.read_sql('select * from s_002174 where dateTime >= "20160408" order by dateTime asc', conn)
     conn.close()
-    df = origDf[['startPrice', 'maxPrice', 'minPrice']].drop(labels=[len(origDf)-1])
-    x = df.values
-    y = origDf[['endPrice']].drop(labels=[0])
+    df = origDf[['startPrice', 'maxPrice', 'endPrice', 'diffPrice', 'diffPercent', 'turnoverAmount', 'amount',
+                 'amplitude', 'turnoverPercent']]
+    predictDays = 10
+    indexToRemoveInX = []
+    indexToRemoveInY = []
+    for i in range(predictDays):
+        indexToRemoveInX.append(len(origDf) - i - 1)
+        indexToRemoveInY.append(i)
+    x = df.drop(labels=indexToRemoveInX).values
+    y = origDf[['endPrice']].drop(labels=indexToRemoveInY).values
     train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.2, shuffle=False)
     lr = LinearRegression(normalize=False, positive=True)
     lr.fit(train_x, train_y)
     print('coef: {}'.format(lr.coef_))
     predictOfTestY = lr.predict(test_x)
-    predictDays = int(math.ceil(0.2 * len(origDf)))
-    print(predictOfTestY)
-
-    # # 组装数据
-    # index = 0
-    # # 在前80%的交易日中，设置预测结果和收盘价一致
-    # while index < len(origDf) - predictDays:
-    #     df.loc[index, 'predictedVal'] = origDf.loc[index, 'endPrice']
-    #     df.loc[index, 'dateTime'] = origDf.loc[index, 'dateTime']
-    #     index = index + 1
-    #
-    # # 在后20%的交易日中，用测试集推算预测股价
-    # predictedCnt = 0
-    # while predictedCnt < predictDays:
-    #     if index >= len(df):
-    #         break
-    #     df.loc[index, 'predictedVal'] = predictOfTestY[predictedCnt]
-    #     df.loc[index, 'dateTime'] = origDf.loc[index, 'dateTime']
-    #     predictedCnt = predictedCnt + 1
-    #     index = index + 1
-
-    # plt.figure()
-    # data1 = {
-    #     "a": [1, 2, 3],
-    #     "b": [4, 5, 6],
-    #     "c": [7, 8, 9]
-    # }
-    # df1 = pd.DataFrame(data1)
-    # # df['predictedVal'].plot(color="red", label='predicted Data')
-    # df1['a'].plot(color="blue", label='Real Data')
-    # plt.legend(loc='best')  # 绘制图例
-    # # 设置x坐标的标签
-    # major_index = origDf.index[origDf.index % 10 == 0]
-    # major_xtics = origDf['dateTime'][origDf.index % 10 == 0]
-    # plt.xticks(major_index, major_xtics)
-    # plt.setp(plt.gca().get_xticklabels(), rotation=30)
-    # # 带网格线，且设置了网格样式
-    # plt.grid(linestyle='-.')
-    # plt.show()
-
+    total = 0
+    for j in range(lr.coef_.size):
+        total = total + lr.coef_[0][j] * test_x[0][j]
+    c = predictOfTestY[0][0] - total
+    print('constant: {}'.format(c))
+    lastPrice = 0
+    for j in range(lr.coef_.size):
+        lastPrice = lastPrice + \
+                    df.values[len(origDf) - predictDays - 1][j] * lr.coef_[0][j]
+    print('last predict price from human calculation: {}'.format(lastPrice + c))
+    print('last predict price from machine calculation: {}'.format(predictOfTestY[len(predictOfTestY) - 1][0]))
+    for i in range(predictDays):
+        predictPrice = 0
+        for j in range(lr.coef_.size):
+            predictPrice = predictPrice + \
+                           df.values[len(origDf) - i - 1][j] * lr.coef_[0][j]
+        print('day {} price: {}'.format(predictDays - i, predictPrice + c))
     endTime = datetime.datetime.now()
     print('end time: {}'.format(endTime))
     print('total time: {}'.format(endTime - startTime))
+
+
+# print(predictOfTestY)
+
+# # 组装数据
+# index = 0
+# # 在前80%的交易日中，设置预测结果和收盘价一致
+# while index < len(origDf) - predictDays:
+#     df.loc[index, 'predictedVal'] = origDf.loc[index, 'endPrice']
+#     df.loc[index, 'dateTime'] = origDf.loc[index, 'dateTime']
+#     index = index + 1
+#
+# # 在后20%的交易日中，用测试集推算预测股价
+# predictedCnt = 0
+# while predictedCnt < predictDays:
+#     if index >= len(df):
+#         break
+#     df.loc[index, 'predictedVal'] = predictOfTestY[predictedCnt]
+#     df.loc[index, 'dateTime'] = origDf.loc[index, 'dateTime']
+#     predictedCnt = predictedCnt + 1
+#     index = index + 1
+
+# plt.figure()
+# data1 = {
+#     "a": [1, 2, 3],
+#     "b": [4, 5, 6],
+#     "c": [7, 8, 9]
+# }
+# df1 = pd.DataFrame(data1)
+# # df['predictedVal'].plot(color="red", label='predicted Data')
+# df1['a'].plot(color="blue", label='Real Data')
+# plt.legend(loc='best')  # 绘制图例
+# # 设置x坐标的标签
+# major_index = origDf.index[origDf.index % 10 == 0]
+# major_xtics = origDf['dateTime'][origDf.index % 10 == 0]
+# plt.xticks(major_index, major_xtics)
+# plt.setp(plt.gca().get_xticklabels(), rotation=30)
+# # 带网格线，且设置了网格样式
+# plt.grid(linestyle='-.')
+# plt.show()
 
 
 try:
