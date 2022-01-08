de0df5d051d6e365e621223814c6074046359a2e
jojopig2002 <jojopig2002@hotmail.com>
2022-01-09 00:42:23 +0800

    tech: draw chart

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import datetime
 
 import pandas as pd
 import pymysql as pymysql
+from matplotlib import pyplot as plt
+from matplotlib.font_manager import FontProperties
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
@@ -17,9 +19,10 @@ def run():
     print('start time: {}'.format(startTime))
     conn = pymysql.connect(host=host, user=user, password=password, db=dbname)
     codes = ['002174', '002460', '300122', '000725', '000100', '002047', '002699', '000665', '600847', '605289']
-    predictDays = 15
-    for i in range(len(codes)):
-        predict(conn, codes[i], predictDays)
+    predictDays = 30
+    # for i in range(len(codes)):
+    #     predict(conn, codes[i], predictDays)
+    predict(conn, '002174', 10)
     conn.close()
     endTime = datetime.datetime.now()
     print('end time: {}'.format(endTime))
@@ -51,54 +54,41 @@ def predict(conn, code, predictDays):
     for j in range(lr.coef_.size):
         lastPrice = lastPrice + \
                     df.values[len(origDf) - predictDays - 1][j] * lr.coef_[0][j]
-    print('last predict price from human calculation: {}'.format(lastPrice + c))
-    print('last predict price from machine calculation: {}'.format(predictOfTestY[len(predictOfTestY) - 1][0]))
+    print('{} predict price from human calculation: {}'.format(origDf.values[len(origDf) - predictDays][1],
+                                                               lastPrice + c))
+    print('{} predict price from machine calculation: {}'.format(origDf.values[len(origDf) - predictDays][1],
+                                                                 predictOfTestY[len(predictOfTestY) - 1][0]))
+    predictDf = pd.DataFrame()
+    realDf = pd.DataFrame()
     for i in range(predictDays):
         predictPrice = 0
         for j in range(lr.coef_.size):
             predictPrice = predictPrice + \
-                           df.values[len(origDf) - i - 1][j] * lr.coef_[0][j]
-        print('stock {} day {} price: {}'.format(origDf[['stockName']].values[0][0], predictDays - i, predictPrice + c))
+                           df.values[len(origDf) - predictDays + i][j] * lr.coef_[0][j]
+            predictDf.loc[i, 'date'] = origDf.values[len(origDf) - predictDays + i, 1][5:]
+            predictDf.loc[i, 'predictEndPrice'] = origDf.values[len(origDf) - predictDays + i, 5]
+            realDf.loc[i, 'date'] = origDf.values[len(origDf) - predictDays + i, 1][5:]
+            realDf.loc[i, 'realEndPrice'] = origDf.values[len(origDf) - predictDays + i, 5]
+            predictDf.loc[i + predictDays, 'date'] = 'Day ' + str(i + 1)
+            predictDf.loc[i + predictDays, 'predictEndPrice'] = predictPrice + c
+        print('stock {} {}, predict day {} price: {}'.format(code, origDf[['stockName']].values[0][0], i + 1,
+                                                             predictPrice + c))
+    draw(code, origDf, predictDf, realDf)
 
 
-# print(predictOfTestY)
-
-# # 组装数据
-# index = 0
-# # 在前80%的交易日中，设置预测结果和收盘价一致
-# while index < len(origDf) - predictDays:
-#     df.loc[index, 'predictedVal'] = origDf.loc[index, 'endPrice']
-#     df.loc[index, 'dateTime'] = origDf.loc[index, 'dateTime']
-#     index = index + 1
-#
-# # 在后20%的交易日中，用测试集推算预测股价
-# predictedCnt = 0
-# while predictedCnt < predictDays:
-#     if index >= len(df):
-#         break
-#     df.loc[index, 'predictedVal'] = predictOfTestY[predictedCnt]
-#     df.loc[index, 'dateTime'] = origDf.loc[index, 'dateTime']
-#     predictedCnt = predictedCnt + 1
-#     index = index + 1
-
-# plt.figure()
-# data1 = {
-#     "a": [1, 2, 3],
-#     "b": [4, 5, 6],
-#     "c": [7, 8, 9]
-# }
-# df1 = pd.DataFrame(data1)
-# # df['predictedVal'].plot(color="red", label='predicted Data')
-# df1['a'].plot(color="blue", label='Real Data')
-# plt.legend(loc='best')  # 绘制图例
-# # 设置x坐标的标签
-# major_index = origDf.index[origDf.index % 10 == 0]
-# major_xtics = origDf['dateTime'][origDf.index % 10 == 0]
-# plt.xticks(major_index, major_xtics)
-# plt.setp(plt.gca().get_xticklabels(), rotation=30)
-# # 带网格线，且设置了网格样式
-# plt.grid(linestyle='-.')
-# plt.show()
+def draw(code, origDf, predictDf, realDf):
+    predictDf = predictDf.sort_index(ascending=True)
+    ax = plt.gca()
+    predictDf.plot(kind='line', x='date', y='predictEndPrice', color='red', ax=ax)
+    realDf.plot(kind='line', x='date', y='realEndPrice', color='blue', ax=ax)
+    font_set = FontProperties(fname=r"/System/Library/Fonts/STHeiti Medium.ttc", size=10)
+    plt.title(code + ' ' + origDf[['stockName']].values[0][0], fontproperties=font_set)
+    major_index = predictDf.index[predictDf.index % 1 == 0]
+    major_xtics = predictDf['date'][predictDf.index % 1 == 0]
+    plt.xticks(major_index, major_xtics)
+    plt.setp(plt.gca().get_xticklabels(), rotation=30)
+    plt.grid(linestyle='dotted')
+    plt.show()
 
 
 try:
